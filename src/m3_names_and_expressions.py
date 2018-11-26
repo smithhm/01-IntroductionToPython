335141a3d3a77068a911e7f2386f381e67a76a7c
smithhm <smithhm@rose-hulman.edu>
2018-11-26 11:18:57 -0500

    DONE

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -1,4 +1,4 @@
-
+import math
 answer = 2 ** 5
 print(answer * 100)
 
@@ -28,7 +28,7 @@ print(answer * 100)
 #     - Prints the square root of that computed value.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
-import math
+# import math (moved to top)
 number = 77 + math.cos(2.75)
 print(number)
 print(math.sqrt(number))
@@ -43,7 +43,7 @@ value = ((41 * 88) + (4 * math.cos(2)))
 print(value)
 print(math.sqrt(value))
 ###############################################################################
-# TODO: 4.
+# DONE: 4.
 #   Immediately below this _TODO_,
 #   write code that computes the square root of 2 in two ways:
 #     - By using the   math.sqrt   function.
@@ -52,9 +52,10 @@ print(math.sqrt(value))
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
 
-
+print(math.sqrt(2))
+print(2**0.5)
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   Every object has a TYPE and a VALUE.  For example,
 #   for the object that is computed by  math.sqrt(2):
 #      Its TYPE is float  (which is shorthand for "floating point number").
@@ -93,10 +94,21 @@ print(math.sqrt(value))
 # After you have written and run the code to learn the TYPE
 # of each of the above, change the above _TODO_ to DONE.
 ###############################################################################
-
+print(type("hello"))
+print(type('hello'))
+print(type('a b c'))
+print(type(3 + 3))
+print(type("3" + "3"))
+print(type(2 ** 100))
+print(type(2.0 ** 100))
+print(type(math.sin(8)))
+print(type(math.sin))
+print(type(print))
+print(type(math))
+print(type('math'))
 ###############################################################################
 #
-# TODO: 6.
+# DONE: 6.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #   Run one more time to be sure that all is still OK.
 #
